af33bcee261398cc67200f5201972eaecab2b3a8
DUMOULIN ANTOINE <antoine.dumoulin2@etu.unistra.fr>
2021-11-23 11:10:02 +0100

    add expr test

---
+++ test/test_expressions.c
@@ -0,0 +1,11 @@
+//Exemple de programme en DECAF
+
+int nombre;
+boolean test;
+
+nombre = 4;
+nombre += 2;
+nombre = nombre + nombre - nombre;
+
+test = nombre == 6 || nombre != 2 && nombre < 10;
+
